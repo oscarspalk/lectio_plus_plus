a94d429144ca7b1634746442240649ea0eca8f3f
Oscar Spalk <oscar.gaardsted.spalk@gmail.com>
2025-06-08 16:41:56 +0200

    ah dammit, i removed the barrelfile package

--- lib/app/logic/student_service.dart
+++ lib/app/logic/student_service.dart
@@ -1,7 +1,5 @@
-import 'package:barrel_files_annotation/barrel_files_annotation.dart';
 import 'package:lectio_wrapper/lectio_wrapper.dart';
 
-@IncludeInBarrelFile()
 class StudentService {
   StudentService({required this.student});
 
--- lib/lectio_plus_plus.dart
+++
@@ -1,4 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-export 'package:lectio_plus_plus/app/logic/student_service.dart'
-    show StudentService;
